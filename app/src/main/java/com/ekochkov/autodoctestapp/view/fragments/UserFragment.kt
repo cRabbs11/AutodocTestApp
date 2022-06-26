52786c26efba53ce5d8ff979de163297c03d6c87
evgenykochkov <crabbs100@gmail.com>
2022-06-26 11:32:50 +0300

    add readme.md, refactoring

--- app/src/main/java/com/ekochkov/autodoctestapp/view/fragments/UserFragment.kt
+++ app/src/main/java/com/ekochkov/autodoctestapp/view/fragments/UserFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.ekochkov.autodoctestapp.R
 import com.ekochkov.autodoctestapp.data.entity.User
@@ -15,10 +14,6 @@ import com.ekochkov.autodoctestapp.databinding.FragmentOwnerBinding
 import com.ekochkov.autodoctestapp.utils.Constants.BUNDLE_KEY_USERNAME
 import com.ekochkov.autodoctestapp.viewModel.UserViewModel
 import com.ekochkov.autodoctestapp.viewModel.factory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.suspendCoroutine
 
 class UserFragment: Fragment() {
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+AutodocTestApp
+===============
+Тестовое задание по созданию приложения для по репозиториям в Github
+
+
+Стэк
+-----------
+
+Использован подход SingleActivity
+
+Стэк: Kotlin, ViewBinding, Retrofit, MVVM, Dagger, Coroutines, Glide
